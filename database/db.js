e320c3a435231c227f140cd4ba5c26cc91354d40
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-05-14 02:29:20 -0700

    mmm

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: process.env.MYSQL_USER,        
   password: process.env.MYSQL_PASSWORD, 
   database: process.env.MYSQL_DB,      
-  port: process.env.MYSQL_PORT || 3306 
+  port: process.env.MYSQL_PORT 
 });
 
 db.connect(err => {
